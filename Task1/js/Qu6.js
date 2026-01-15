0bb1e3d2b31f1593f207cd72bce4c3b55bf4b09b
Jason Lee <jason.m.lee.2003@gmail.com>
2026-01-15 17:04:40 -0500

    Added text object

--- Task1/js/Qu6.js
+++ Task1/js/Qu6.js
@@ -1,9 +1,17 @@
 "use structs";
 
+const text = {
+name: "test",
+color: [255],
+textSize: 28,
+positionX: 400,
+positionY: 300,
+} 
+
 function setup() {
     createCanvas(800, 600);
 }
 
 function draw() {
-    background(220);
+background(255);
 }
