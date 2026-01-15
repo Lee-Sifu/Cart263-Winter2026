891d3fb9f47a1c2551774d09bdd911b8806cfe25
Jason Lee <jason.m.lee.2003@gmail.com>
2026-01-15 14:54:37 -0500

    Added rectangle variables

--- Task1/Qu3.js
+++ Task1/Qu3.js
@@ -1,9 +1,31 @@
 "use struct";
 
+let rect1 = {
+    x: 150,
+    y: 150,
+    w: 100,
+    h: 50,
+    color: [255, 200, 0]
+}
+let rect2 = {
+    x: 400,
+    y: 300,
+    w: 150,
+    h: 75,
+    color: [0, 200, 255]
+}
+let rect3 = {
+    x: 650,
+    y: 450,
+    w: 200,
+    h: 100,
+    color: [200, 0, 255]
+}
+
 function setup() {
     createCanvas(800, 600);
 }
 
 function draw() {
-    
+
 }
