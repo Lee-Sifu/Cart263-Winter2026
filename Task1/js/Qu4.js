0d0baa4a70e54119a1010fcd89fb615487e2bf5a
Jason Lee <jason.m.lee.2003@gmail.com>
2026-01-15 15:53:47 -0500

    Added the rectangle and chosen blue colors

--- Task1/js/Qu4.js
+++ Task1/js/Qu4.js
@@ -1,9 +1,31 @@
 "use struct";
 
+const canvasWidth = 800;
+const canvasHeight = 600;
+
+const rectWidth = canvasWidth / 3;
+const rectHeight = canvasHeight;
+
+const rect1X = 0;
+const rect2X = rectWidth;
+const rect3X = rectWidth * 2;
+
+const rect1Color = {r: 0, g: 255, b: 255};
+const rect2Color = {r: 0, g: 0, b: 255};
+const rect3Color = {r: 150, g: 222, b: 209};
+
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(canvasWidth, canvasHeight);
 }
 
 function draw() {
-    
+    background(0);
+    fill(rect1Color.r, rect1Color.g, rect1Color.b);
+    rect(rect1X, 0, rectWidth, rectHeight);
+
+    fill(rect2Color.r, rect2Color.g, rect2Color.b);
+    rect(rect2X, 0, rectWidth, rectHeight);
+
+    fill(rect3Color.r, rect3Color.g, rect3Color.b);
+    rect(rect3X, 0, rectWidth, rectHeight);
 }
