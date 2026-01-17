07281672bff57fcdb85e8a5727eba4bcb7aa5c98
Jason Lee <jason.m.lee.2003@gmail.com>
2026-01-17 15:53:56 -0500

    Added the text object

--- Task1/js/Qu6.js
+++ Task1/js/Qu6.js
@@ -1,17 +1,28 @@
 "use strict";
 
-const text = {
-name: "test",
-color: [255],
-textSize: 28,
-positionX: 400,
-positionY: 300,
-} 
+const myText = {
+    name: "test",
+    color: [255],
+    textSize: 28,
+    positionX: 400,
+    positionY: 300,
+}
 
 function setup() {
     createCanvas(800, 600);
 }
 
 function draw() {
-background(255);
+    background(0, 0, 0);
+    let i = 0;
+    for (i = 0; i < 1; i++) {
+        displayText(i);
+    }
+}
+
+function displayText(index) {
+    fill(myText.color);
+    textSize(myText.textSize);
+    textAlign(CENTER, CENTER);
+    text(myText.name, myText.positionX + index * 20, myText.positionY);  
 }
