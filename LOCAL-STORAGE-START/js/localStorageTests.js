0d6e67cce6d9ccdb1c0ddc1c4637c9603a41d402
Jason Lee <153792289+Lee-Sifu@users.noreply.github.com>
2026-02-12 14:37:28 -0500

    Add files via upload

---
+++ LOCAL-STORAGE-START/js/localStorageTests.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+    console.log("in local storage ex");
+let theButtons = this.document.getElementsByClassName("titleBar");
+for (let i = 0; i < theButtons.length; i++) {
+    theButtons[i].addEventListener("click", saveStateHandler);
+}
+// the callback function
+  function saveStateHandler(event) {
+   console.log(this.parentElement); // get parent element
+    let buttonID = this.parentElement.id;
+    let inputValue = this.parentElement.querySelector("input").value;
+    console.log(inputValue);
+
+    if (inputValue !== "") {
+        localStorage.setItem(buttonID, inputValue);
+        inputalue = "";
+        console.log(inputValue);
+    }
+  }
+  this.document.getElementById("refresh").addEventListener("click", retrieveHandler);
+  function retrieveHandler(){
+    for (let [key, value] of Object.entries(localStorage)){
+        let textBox = document.querySelector('div[data-ref=${key}]');
+        textBox.innerHTML = value;
+    }
+  }
+};
